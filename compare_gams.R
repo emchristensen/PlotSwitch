79a8d569cdb4d1220f73e4a20285b51b89ad8ed5
Erica Christensen <erica.christensen@weecology.org>
2017-08-17 13:12:56 -0400

    fixed minor bugs in compare_gams.R

--- compare_gams.R
+++ compare_gams.R
@@ -4,6 +4,7 @@ library(mgcv)
 library(ggplot2)
 
 source('gam_functions.R')
+source('data_functions.R')
 
 ##### Items for all Dipo analyses
 dipo_data = make_dipo_data()
@@ -14,13 +15,13 @@ Ylab = 'Dipodomys abundance/plot'
 ##### CC Plots 
 CC = dipo_data %>% filter(treatment == "CC") %>% arrange(date)
 CC$plot = as.factor(CC$plot)
-m_CC_plot <- gamm(DipoN ~ plot + s(month, bs = "cc", k = 12) + s(Time), data = CC)
-m_CC <- gamm(DipoN ~ s(month, bs = "cc", k = 12) + s(Time), data = CC)
+m_CC_plot <- gam(DipoN ~ plot + s(month, bs = "cc", k = 12) + s(Time), data = CC)
+m_CC <- gam(DipoN ~ s(month, bs = "cc", k = 12) + s(Time), data = CC)
 
 gam_diagnostics(m_CC_plot, "Plot in model")
 
 
-CC_trend = make_prediction(CC,m_CC)
+CC_trend = make_prediction_gam(CC,m_CC,colname='DipoN')
 op <- par(mar = c(5,4,2,2) + 0.1)
 
 # plot gam results
