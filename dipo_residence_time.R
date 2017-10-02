5c3188cf7ef9f21def8e2facfbc7b322c62740cf
Erica Christensen <erica.christensen@weecology.org>
2017-10-02 16:58:04 -0400

    refactored dipo_residence_time.R -- wrote function

--- dipo_residence_time.R
+++ dipo_residence_time.R
@@ -2,68 +2,88 @@
 library(dplyr)
 
 
-dat = read.csv('C:/Users/EC/Desktop/git/PortalData/Rodents/Portal_rodent.csv',stringsAsFactors = F)
+
+
+#' @param tags list of unique tag numbers
+#' @param dat data frame in which to look for captures
+#' @return returns data frame with tag, first_cap, last_cap [dates], and moved [0/1 indicates whether the animal was captured on more than one plot]
+first_last_cap = function(tags,dat) {
+  df = c()
+  for (indiv in tags) {
+    encounters = filter(dat, tag==indiv)
+    first = filter(encounters,period==min(encounters$period))
+    first_cap = as.Date(paste(first$year,first$month,first$day,sep='-'))
+    last = filter(encounters,period==max(encounters$period))
+    last_cap = as.Date(paste(last$year,last$month,last$day,sep='-'))
+    moved = 0
+    if (length(unique(encounters$plot))>1) {moved = 1}
+    df = rbind(df,c(indiv,first_cap,last_cap,moved))
+  }
+  df = data.frame(df,stringsAsFactors = F)
+  names(df) = c('tag','first_cap','last_cap','moved')
+  df$span = as.numeric(df$last_cap) - as.numeric(df$first_cap)
+  return(df)
+}
+
 
 
 # what is average apparent life span of dipo species for the year before the switch
-dipo_before = filter(dat,period>=424,period<=435,species %in% c('DM')) 
+dat = read.csv('C:/Users/EC/Desktop/git/PortalData/Rodents/Portal_rodent.csv',stringsAsFactors = F)
+newdipodat = filter(dat, period>0, species=='DM', note2=='*', prevrt=='', prevlet=='')
 
-# find tag numbers for animals newly tagged in first 3 months
-new_before = filter(dipo_before,note2=='*',period %in% c(424:426),prevrt=='',prevlet=='',plot %in% c(4,11,14,17)) %>% select(tag)
+# find tag numbers for animals newly tagged in March-May
+new_before_cc = filter(newdipodat, period %in% c(425:427), plot %in% c(4,11,14,17))
 
-RT_before = data.frame()
-for (indiv in new_before$tag) {
-  encounters = filter(dipo_before,tag==indiv)
-  residence = max(encounters$period) - min(encounters$period)
-  moved = 0
-  if (length(unique(encounters$plot))>1) {moved=1}
-  RT_before = rbind(RT_before,c(residence,moved))
-}
-names(RT_before) = c('res_time','moved')
-hist(RT_before[,1])  
+RT_before = first_last_cap(new_before_cc$tag,dat)
+hist(RT_before$span)  
 
 # ==================================================================
 # long term controls after
-dipo_after = filter(dat,period>=436,period<=447,species=='DM')
 
-new_cc_after = filter(dipo_after,note2=='*',period %in% c(436:438),prevrt=='',prevlet=='',plot %in% c(4,11,14,17)) %>% select(tag)
+new_cc_after = filter(newdipodat, period %in% c(437:439), plot %in% c(4,11,14,17))
+RT_after_cc = first_last_cap(new_cc_after$tag,dat)
 
-RT_after = data.frame()
-for (indiv in new_cc_after$tag) {
-  encounters = filter(dipo_after,tag==indiv)
-  residence = max(encounters$period) - min(encounters$period)
-  moved=0
-  if (length(unique(encounters$plot))>1) {moved=1}
-  RT_after = rbind(RT_after,c(residence,moved))
-}
-names(RT_after) = c('res_time','moved')
-hist(RT_after$res_time)
+hist(RT_after_cc$span)
+
 
 # ==================================================================
 # exclosures to controls
-new_xc_after = filter(dipo_after,note2=='*',period %in% c(436:438),prevrt=='',prevlet=='',plot %in% c(5,7,24)) %>% select(tag)
+new_xc_after = filter(newdipodat, period %in% c(437:439), plot %in% c(5,7,24))
+RT_after_xc = first_last_cap(new_xc_after$tag,dat)
 
-RT_after_xc = data.frame()
-for (indiv in new_xc_after$tag) {
-  encounters = filter(dipo_after,tag==indiv)
-  residence = max(encounters$period) - min(encounters$period)
-  moved=0
-  if (length(unique(encounters$plot))>1) {moved=1}
-  RT_after_xc = rbind(RT_after_xc,c(residence,moved))
-}
-names(RT_after_xc) = c('res_time','moved')
-hist(RT_after_xc$res_time)
-}
+hist(RT_after_xc$span)
 
-new_ec_after = filter(dipo_after,note2=='*',period %in% c(436:438),prevrt=='',prevlet=='',plot %in% c(6,13,18)) %>% select(tag)
 
-RT_after_ec = data.frame()
-for (indiv in new_ec_after$tag) {
-  encounters = filter(dipo_after,tag==indiv)
-  residence = max(encounters$period) - min(encounters$period)
-  moved=0
-  if (length(unique(encounters$plot))>1) {moved=1}
-  RT_after_ec = rbind(RT_after_ec,c(residence,moved))
-}
-names(RT_after_ec) = c('res_time','moved')
-hist(RT_after_ec$res_time)
+new_ec_after = filter(newdipodat, period %in% c(437:439), plot %in% c(6,13,18))
+RT_after_ec = first_last_cap(new_ec_after$tag,dat)
+
+hist(RT_after_ec$span)
+
+
+# ==================================================================
+# compare metrics
+
+# how many newly tagged animals in span
+length(RT_before$span)
+length(RT_after_cc$span)
+length(RT_after_xc$span)
+length(RT_after_ec$span)
+
+# average life span of animals captured more than once
+filter(RT_before,span>0) %>% select(span) %>% colMeans()
+filter(RT_after_cc,span>0) %>% select(span) %>% colMeans()
+filter(RT_after_xc,span>0) %>% select(span) %>% colMeans()
+filter(RT_after_ec,span>0) %>% select(span) %>% colMeans()
+
+# percent of animals captured more than once
+length(RT_before$span[RT_before$span>0])/length(RT_before$span)
+length(RT_after_cc$span[RT_after_cc$span>0])/length(RT_after_cc$span)
+length(RT_after_xc$span[RT_after_xc$span>0])/length(RT_after_xc$span)
+length(RT_after_ec$span[RT_after_ec$span>0])/length(RT_after_ec$span)
+
+# percent of animals that moved plot
+length(RT_before$moved[RT_before$moved==1])/length(RT_before$moved)
+length(RT_after_cc$moved[RT_after_cc$moved==1])/length(RT_after_cc$moved)
+length(RT_after_xc$moved[RT_after_xc$moved==1])/length(RT_after_xc$moved)
+length(RT_after_ec$moved[RT_after_ec$moved==1])/length(RT_after_ec$moved)
+
