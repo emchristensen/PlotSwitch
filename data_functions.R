036cb7f3218d8bc0d32fc2546e0d5b43d00cdc06
skmorgane <morgan@weecology.org>
2017-11-06 09:18:42 -0500

    Portalr friendly rodent abundance processing
    This commit finishes the process of taking the original hand-crafted rodent data extraction and processing and makes a process built on the portalr package. Still to do: remove get data from the get_rodent so that data extraction doesn't have to be repeated for each type of data summary one wishes.

--- data_functions.R
+++ data_functions.R
@@ -1,11 +1,28 @@
 library(dplyr)
 library(portalr)
 
+#################################################
+# TO DO:
+# 1) In the original code for creating GAM data, period 457 was added back into the
+#    data. It is an incomplete census, but all the plots needed for this analysis
+#    were sampled. In refactoring the code to work with portalr, SKME did not
+#    add back in 457 only because doing so would be a pain. Adding 457 still 
+#    needs to be done. Below is the orginal line of code on this issue. It won't
+#    work not because of the way portalr is being used:
+#    fullcensus = plotstrapped[plotstrapped$x>=21,] 
+#    warning: this may not be ok for other projects, period 457 only trapped 21 
+#    plots but the skipped ones aren't relevant to this project
+
+# test code: 
+# dat = get_data(startdate = "2013-03-11")
+# dat = make_data(species="SmM")
+#################################################
+
 #' @title Get Rodent Data
 #' 
 #' @description make a data frame for running GAM models
 #' 
-#' @param
+#' @param startdate Census date of the period code used to filter the data
 #' 
 #' @return data frame with columns:
 #'            plot
@@ -16,14 +33,55 @@ library(portalr)
 #'            treatment (two letters representing before/after switch: C = control, E = krat exclosure, X = total rodent removal)
 
 
-get_data = function(){
-  start_date = "2013-03-11"
+get_data = function(startdate){
   data = portalr::abundance(path='repo', level = 'Plot', type='Granivores',
                             length="All", unknowns=TRUE, incomplete=FALSE,
                             shape="flat", time='date')
   data$numericdate = as.numeric(data$censusdate) / 1000
-  rdat_filtered = dplyr::filter(data, censusdate >= start_date)
+  rdat_filtered = dplyr::filter(data, censusdate >= startdate)
   rdat_filtered = add_treatment(rdat_filtered)
+}
+
+#' @title Make Data
+#'
+#' @description make a data frame of rodent abundances for GAM models
+#' 
+#' @param species species desired; either "All," "Granivore," "Dipos," "SmGran," "SmH" (small heteromyid), or "SmM" (small murid)
+#' @param start_period first period number of data desired; default is 415 (2013)
+#' 
+#' @return data frame with columns:
+#'            plot
+#'            censusdate (date of data collection formatted YYYY-MM-DD)
+#'            treatment (two letters representing before/after switch: C = control, E = krat exclosure, X = total rodent removal)
+#'            n (number of individuals)
+#'            Time (numeric version of censusdate for GAM analyses)
+
+make_data= function(species='All',start_date = "2013-03-11") {
+  # Create species abundances by plot by period
+  dat = get_data(startdate=start_date)
+  # select species of interest
+  if (species=='All') {targetsp = c('BA','DM','DO','DS','NA','OL','OT','PB','PE','PF','PM','PP','RM','RO','SF','SH')}
+  if (species=='Granivore') {targetsp = c('BA','DM','DO','DS','PB','PE','PF','PH','PI','PL','PM','PP','RF','RM','RO')}
+  if (species=='SmGran') {targetsp = c('BA','PB','PE','PF','PH','PI','PL','PM','PP','RF','RM','RO')}
+  if (species=='SmH') {targetsp = c('PB','PF','PH','PI','PP')}
+  if (species=='SmM') {targetsp = c('BA','PE','PL','PM','RF','RM','RO')}
+  if (species=='Dipos') {targetsp = c('DM','DO','DS')}
+  
+  target_dat = dplyr::filter(dat,species %in% targetsp)
+  total = aggregate(target_dat$abundance,
+                    by=list(plot=target_dat$plot,censusdate=target_dat$censusdate,
+                            Time=target_dat$numericdate, 
+                            treatment=target_dat$treatment),
+                    FUN=sum)
+  
+  total$x[is.na(total$x)] = 0
+  # change column name
+  total_gam = rename(total,n=x)
+  # put data in chronological order
+  total_gam = total_gam[order(total_gam$Time),]
+  trt_files = trt_data(total_gam)
+  
+  return(trt_files)
 }
 
 #' @title Add treatment codes
@@ -51,132 +109,27 @@ add_treatment = function(data){
   return(data)
 }
 
-#Things done in the old code that are not currently implemented below this line:
 
-fullcensus = plotstrapped[plotstrapped$x>=21,] # warning: this may not be ok for other projects, period 457 only trapped 21 plots but the skipped ones aren't relevant to this project
-  
-
-
-
-#' @title Make Dipo Data
+#' @title Separate Treatment Data
 #' 
-#' @description make a data frame for running GAM models
+#' @description takes data with muyltiple treatment and separates into distinct files for analysis
 #' 
-#' @param
-#' 
-#' @return data frame with columns:
-#'            plot
-#'            period
-#'            treatment (two letters representing before/after switch: C = control, E = krat exclosure, X = total rodent removal)
-#'            DipoN (number of DM, DO, DS summed)
-#'            date 
-#'            month
-#'            Year
-#'            Time
-
-make_dipo_data = function(){
-  # Create species abundances by plot by period
-  byspecies = rodent_abundance(start_period=415,incomplete=F)
-  
-  # number of dipos per plot
-  d = byspecies %>% 
-    filter(period>414, species %in% c('DM','DO','DS'))
-  dipos = aggregate(d$x, by=list(period=d$period, plot=d$plot, date=d$date, month=d$month, year=d$year, Time=d$Time), FUN=sum)
-  # data frame of all plots in all periods
-  allplotsperiod = expand.grid(period=unique(dipos$period), plot=unique(dipos$plot))
-  #attach date columns according to period
-  allplotsperiod = append_dates(allplotsperiod)
-  # attach treatment column according to plot number
-  treatment = data.frame(treatment = c('CX','CE','EE','CC',
-                                       'XC','EC','XC','CE',
-                                       'CX','XX','CC','CX',
-                                       'EC','CC','EE','XX',
-                                       'CC','EC','EE','EE',
-                                       'EE','CE','XX','XC'),plot=seq(1,24))
-  allplotsperiod = merge(allplotsperiod,treatment)
-  # merge capture data with data frame of all plots and all periods, and fill in empty data with zeros
-  dipos = merge(allplotsperiod,dipos,all=T)
-  dipos$x[is.na(dipos$x)] = 0
-  # change column name
-  dipos_gam = rename(dipos,DipoN=x)
-  # put data in chronological order
-  dipos_gam = dipos_gam[order(dipos_gam$period),]
-  
-  return(dipos_gam)
-}
-
-#' @title Make Data
-#'
-#' @description make a data frame for running GAM models
-#' 
-#' @param species species desired; either "All," "Granivore," "Dipos," "SmGran," "SmH" (small heteromyid), or "SmM" (small murid)
-#' @param start_period first period number of data desired; default is 415 (2013)
-#' 
-#' @return data frame with columns:
-#'            plot
-#'            period
-#'            treatment (two letters representing before/after switch: C = control, E = krat exclosure, X = total rodent removal)
-#'            n (number of small heteromyids)
-#'            date 
-#'            month
-#'            Year
-#'            Time
-
-make_data= function(species='All',start_period=415) {
-  # Create species abundances by plot by period
-  dat = rodent_abundance(start_period=start_period,incomplete=F)
-  # select species of interest
-  if (species=='All') {targetsp = c('BA','DM','DO','DS','NA','OL','OT','PB','PE','PF','PM','PP','RM','RO','SF','SH')}
-  if (species=='Granivore') {targetsp = c('BA','DM','DO','DS','PB','PE','PF','PH','PI','PL','PM','PP','RF','RM','RO')}
-  if (species=='SmGran') {targetsp = c('BA','PB','PE','PF','PH','PI','PL','PM','PP','RF','RM','RO')}
-  if (species=='SmH') {targetsp = c('PB','PF','PH','PI','PP')}
-  if (species=='SmM') {targetsp = c('BA','PE','PL','PM','RF','RM','RO')}
-  if (species=='Dipos') {targetsp = c('DM','DO','DS')}
-  
-  target_dat = filter(dat,species %in% targetsp)
-  total = aggregate(target_dat$x,by=list(period=target_dat$period,plot=target_dat$plot,date=target_dat$date,month=target_dat$month,
-                                         year=target_dat$year,Time=target_dat$Time),FUN=sum)
-
-  # data frame of all plots in all periods
-  allplotsperiod = expand.grid(period=unique(dat$period), plot=unique(dat$plot))
-  #attach date columns according to period
-  allplotsperiod = append_dates(allplotsperiod)
-  # attach treatment column according to plot number
-  treatment = data.frame(treatment = c('CX','CE','EE','CC',
-                                       'XC','EC','XC','CE',
-                                       'CX','XX','CC','CX',
-                                       'EC','CC','EE','XX',
-                                       'CC','EC','EE','EE',
-                                       'EE','CE','XX','XC'),plot=seq(1,24))
-  allplotsperiod = merge(allplotsperiod,treatment)
-  # merge capture data with data frame of all plots and all periods, and fill in empty data with zeros
-  total = merge(allplotsperiod,total,all=T)
-  total$x[is.na(total$x)] = 0
-  # change column name
-  total_gam = rename(total,n=x)
-  # put data in chronological order
-  total_gam = total_gam[order(total_gam$period),]
-  
-  return(total_gam)
-}
-
-#' @title Treatment Data
-#' 
-#' @description takes tallies of dipos from make_dipo_data and separates by treatment
-#' 
-#' @param data dataframe output from make_dipo_data
+#' @param data dataframe output from make_data
 #' 
 #' @return a list containing three data frames, containing dipo data from 3 treatment types
 
 trt_data = function(data){
   data$plot = as.factor(data$plot)
-  CC = data %>% filter(treatment == "CC") %>% arrange(date)
-  EC = data %>% filter(treatment == "EC") %>% arrange(date)
-  XC = data %>% filter(treatment == "XC") %>% arrange(date)
-  CE = data %>% filter(treatment == "CE") %>% arrange(date)
-  EE = data %>% filter(treatment == "EE") %>% arrange(date)
-  return(list(CC,EC,XC,CE,EE))
+  CC = dplyr::filter(data, treatment == "CC") %>% dplyr::arrange(Time)
+  EC = dplyr::filter(data, treatment == "EC") %>% dplyr::arrange(Time)
+  XC = dplyr::filter(data, treatment == "XC") %>% dplyr::arrange(Time)
+  return(list(CC,EC,XC))
 }
+
+
+
+
+  
 
 
 
@@ -315,4 +268,35 @@ get_evenness = function(start_period=415) {
   evenness_dat = evenness_dat[order(evenness_dat$period),]
   
   return(evenness_dat)
+}
+
+make_dipo_data = function(){
+  # Create species abundances by plot by period
+  byspecies = rodent_abundance(start_period=415,incomplete=F)
+  
+  # number of dipos per plot
+  d = byspecies %>% 
+    filter(period>414, species %in% c('DM','DO','DS'))
+  dipos = aggregate(d$x, by=list(period=d$period, plot=d$plot, date=d$date, month=d$month, year=d$year, Time=d$Time), FUN=sum)
+  # data frame of all plots in all periods
+  allplotsperiod = expand.grid(period=unique(dipos$period), plot=unique(dipos$plot))
+  #attach date columns according to period
+  allplotsperiod = append_dates(allplotsperiod)
+  # attach treatment column according to plot number
+  treatment = data.frame(treatment = c('CX','CE','EE','CC',
+                                       'XC','EC','XC','CE',
+                                       'CX','XX','CC','CX',
+                                       'EC','CC','EE','XX',
+                                       'CC','EC','EE','EE',
+                                       'EE','CE','XX','XC'),plot=seq(1,24))
+  allplotsperiod = merge(allplotsperiod,treatment)
+  # merge capture data with data frame of all plots and all periods, and fill in empty data with zeros
+  dipos = merge(allplotsperiod,dipos,all=T)
+  dipos$x[is.na(dipos$x)] = 0
+  # change column name
+  dipos_gam = rename(dipos,DipoN=x)
+  # put data in chronological order
+  dipos_gam = dipos_gam[order(dipos_gam$period),]
+  
+  return(dipos_gam)
 }
