356a91dbf6b280cf357650662391da98504e50b4
Erica Christensen <erica.christensen@weecology.org>
2018-08-15 09:32:31 -0400

    make functions for plant_ordination_analysis, add code for analyzing pre-flip rodent differences in pre-flip_differences

--- plant_ordination_analysis.R
+++ plant_ordination_analysis.R
@@ -5,98 +5,177 @@
 # another example: https://rgriff23.github.io/2017/05/23/mosquito-community-ecology-in-vegan.html
 
 
+
 library(dplyr)
 library(vegan)
 library(ggplot2)
 
-# data
-dat.winter <- read.csv('WinterAnnualTreatments2.csv')
+# functions ----
+#' @title plant pcca
+#' 
+#' @description prepares plant data for pcca and returns cca object
+#' @param plantdat matrix of species counts by plot and year
+#'
+plant_pcca = function(plantdat) {
+  # sqrt transofrm abundance data to account for huge differences in abundance year to year
+  species = sqrt(as.matrix(plantdat[,!names(plantdat) %in% c('year','season','plot','flip_type','treat_before','treat_after')]))
+  year = as.factor(plantdat$year)
+  trt = as.factor(plantdat$treat_before)
+  plot = as.factor(plantdat$plot)
+  
+  # pcca (constrained by year)
+  plant.pcca <- cca(species ~ trt + Condition(year))
+  
+  return(plant.pcca)
+}
+
+#' @title plot pcca with ellipses
+#' @param pcca.obj result of pcca analysis
+#' @param plantdat original matrix of plant count data used in pcca analysis
+#' @param title string; title of plot
+#' 
+plot_pcca_ellipses = function(pcca.obj, plantdat,title) {
+  results=data.frame(scores(pcca.obj)$sites,
+                      year=plantdat$year,
+                      plot=plantdat$plot,
+                      treatment=plantdat$treat_before)
+  plotobj=ggplot(results,aes(CCA1, CA1)) + 
+    stat_ellipse(aes(color = treatment)) +
+    geom_point(aes(color = treatment)) +
+    #geom_text(aes(label = plot, color = as.factor(year))) +
+    coord_cartesian(xlim = c(-5, 4), ylim = c(-7, 7)) +
+    theme(legend.title=element_blank()) +
+    #scale_color_discrete(direction=-1) +
+    scale_colour_brewer(type = 'qual', palette = 'Dark2',
+                        breaks=c("control","exclosure","removal"),
+                        labels=c("long-term\ncontrol\n", "kangaroo rat\nremoval\n", "rodent\nremoval")) +
+    ggtitle(title)
+  return(plotobj)
+}
+
+# data ----
+# Data I'm using is all censuses before flip, back to 2008 (many summers were missed)
+# Summer: 2008, 2012, 2014 and Winter: 2008, 2012, 2013, 2014, 2015
+
+dat.winter <- read.csv('WinterAnnualTreatments.csv')
 dat.summer <- read.csv('SummerAnnualTreatments.csv')
 
 # ===================================================================
 # Do plant communities differ by treatment 2008-2015?
-# 
 
-# pcca model
-# sqrt transofrm abundance data to account for huge differences in abundance year to year
-win.species = sqrt(as.matrix(dat.winter[,!names(dat.winter) %in% c('year','season','plot','flip_type','treat_before','treat_after')]))
+# krat exclosures vs removals ----
+dat.winter1 = dplyr::filter(dat.winter,treat_before %in% c('exclosure','removal'))
+dat.summer1 = dplyr::filter(dat.summer,treat_before %in% c('exclosure','removal'))
 
-win.year = as.factor(dat.winter$year)
-win.trt = as.factor(dat.winter$treat_before)
-win.plot = as.factor(dat.winter$plot)
+# pcca model: winter
+win.pcca1 = plant_pcca(dat.winter1)
 
-# pcca (constrained by year)
-win.pcca <- cca(win.species ~ win.trt + Condition(win.year))
-
-vif.cca(win.pcca)
-anova(win.pcca)
-permutest(win.pcca,permutations=500) 
+vif.cca(win.pcca1)
+anova(win.pcca1)
+permutest(win.pcca1,permutations=500) # should be similar to anova on pcca
+#anova(win.pcca,strata=win.year) # more conservative test   
 
 # proportion of variance explained
-win.pcca$CCA$tot.chi/win.pcca$tot.chi
-
-
-### ADONIS - another test to look for compositional differences, with similar results to above.(from Supp et al 2012)
-# winter
-win.spp.canb = vegdist(win.species, method = "canb")
-win.canb = adonis(win.spp.canb ~ win.trt, permutation=1000)
-win.canb
-
-
-# From the anova and permutest it seems that treatment is significant, but explains a tiny amount of variance.
+win.pcca1$CCA$tot.chi/win.pcca1$tot.chi
 
 # plot with year, plot, treatment
-results=data.frame(scores(win.pcca)$sites,
-                   year=win.year,
-                   plot=win.plot,
-                   treatment=win.trt)
-ggplot(results,aes(CCA1, CCA2)) + 
-  stat_ellipse(aes(color = treatment)) +
-  geom_text(aes(label = plot, color = as.factor(year))) +
-  coord_cartesian(xlim = c(-5, 4), ylim = c(-7, 7)) +
-  theme(legend.title=element_blank()) +
-  scale_color_discrete(direction=-1)
+excl_rem_win = plot_pcca_ellipses(win.pcca1, dat.winter1,'Winter Annual Plants')
 
+#ggsave('Winter_Exclosure_Removal.png',excl_rem_win,width=4,height=3)
 
-# ==========================================================================================================
-# Summer annuals
-# pcca model
-# sqrt transofrm abundance data to account for huge differences in abundance year to year
-sum.species = sqrt(as.matrix(dat.summer[,!names(dat.summer) %in% c('year','season','plot','flip_type','treat_before','treat_after')]))
+# pcca model: summer
+sum.pcca1 = plant_pcca(dat.summer1)
 
-sum.year = as.factor(dat.summer$year)
-sum.trt = as.factor(dat.summer$treat_before)
-sum.plot = as.factor(dat.summer$plot)
-
-# pcca (constrained by year)
-sum.pcca <- cca(sum.species ~ sum.trt + Condition(sum.year))
-
-vif.cca(sum.pcca)
-anova(sum.pcca)
-permutest(sum.pcca,permutations=500) 
+vif.cca(sum.pcca1)
+anova(sum.pcca1)
+permutest(sum.pcca1,permutations=500) 
 
 # proportion of variance explained
-sum.pcca$CCA$tot.chi/sum.pcca$tot.chi
-
-
-### ADONIS - another test to look for compositional differences, with similar results to above.(from Supp et al 2012)
-# winter
-sum.spp.canb = vegdist(sum.species, method = "canb")
-sum.canb = adonis(sum.spp.canb ~ sum.trt, permutation=1000)
-sum.canb
-
-
-# From the anova and permutest it seems that treatment is significant, but explains a tiny amount of variance.
+sum.pcca1$CCA$tot.chi/sum.pcca1$tot.chi
 
 # plot with year, plot, treatment
-results2=data.frame(scores(sum.pcca)$sites,
-                   year=sum.year,
-                   plot=sum.plot,
-                   treatment=sum.trt)
-ggplot(results2,aes(CCA1, CCA2)) + 
-  stat_ellipse(aes(color = treatment)) +
-  geom_text(aes(label = plot, color = as.factor(year))) +
-  coord_cartesian(xlim = c(-5, 4), ylim = c(-7, 7)) +
-  theme(legend.title=element_blank()) +
-  scale_color_discrete(direction=-1)
+excl_rem_sum = plot_pcca_ellipses(sum.pcca1, dat.summer1,'Summer Annual Plants')
+excl_rem_sum
+ggsave('Summer_Exclosure_Removal.png',excl_rem_sum,width=4,height=3)
 
+
+# controls vs total rodent removals ----
+dat.winter3 = dplyr::filter(dat.winter,treat_before %in% c('removal','control'))
+dat.summer3 = dplyr::filter(dat.summer,treat_before %in% c('removal','control'))
+
+# pcca model: winter
+win.pcca3 = plant_pcca(dat.winter3)
+
+vif.cca(win.pcca3)
+anova(win.pcca3)
+permutest(win.pcca3,permutations=500) # should be similar to anova on pcca
+#anova(win.pcca3,strata=dat.winter3$year) # more conservative test   
+
+# proportion of variance explained
+win.pcca3$CCA$tot.chi/win.pcca3$tot.chi
+
+# plot with year, plot, treatment
+ctrl_rem_win = plot_pcca_ellipses(win.pcca3, dat.winter3,'Winter Annual Plants')
+ctrl_rem_win
+ggsave('Winter_Control_Removal.png',ctrl_rem_win,width=4,height=3)
+
+# pcca: summer
+sum.pcca3 = plant_pcca(dat.summer3)
+
+vif.cca(sum.pcca3)
+anova(sum.pcca3)
+permutest(sum.pcca3,permutations=500)
+#anova(sum.pcca3,strata=dat.summer3$year) # more conservative test 
+
+# proportion of variance explained
+sum.pcca3$CCA$tot.chi/sum.pcca3$tot.chi
+
+# plot with year, plot, treatment
+ctrl_rem_sum = plot_pcca_ellipses(sum.pcca3, dat.summer3,'Summer Annual Plants')
+ctrl_rem_sum
+ggsave('Summer_Control_Removal.png',ctrl_rem_sum,width=4,height=3)
+
+
+# controls vs krat exclosures ----
+dat.winter2 = dplyr::filter(dat.winter,treat_before %in% c('exclosure','control'))
+dat.summer2 = dplyr::filter(dat.summer,treat_before %in% c('exclosure','control'))
+
+# pcca model: winter
+win.pcca2 = plant_pcca(dat.winter2)
+
+vif.cca(win.pcca2)
+anova(win.pcca2)
+permutest(win.pcca2,permutations=500) # should be similar to anova on pcca
+#anova(win.pcca2,strata=dat.winter2$year) # more conservative test   
+
+# proportion of variance explained
+win.pcca2$CCA$tot.chi/win.pcca2$tot.chi
+
+# plot with year, plot, treatment
+ctrl_excl_win = plot_pcca_ellipses(win.pcca2, dat.winter2,'Winter Annual Plants')
+ctrl_excl_win
+ggsave('Winter_Control_Exclosure.png',ctrl_excl_win,width=4,height=3)
+
+# pcca: summer
+sum.pcca2 = plant_pcca(dat.summer2)
+
+vif.cca(sum.pcca2)
+anova(sum.pcca2)
+permutest(sum.pcca2,permutations=500)
+#anova(sum.pcca2,strata=dat.summer2$year) # more conservative test 
+
+# proportion of variance explained
+sum.pcca2$CCA$tot.chi/sum.pcca2$tot.chi
+
+# plot with year, plot, treatment
+ctrl_excl_sum = plot_pcca_ellipses(sum.pcca2, dat.summer2,'Summer Annual Plants')
+ctrl_excl_sum
+ggsave('Summer_Control_Exclosure.png',ctrl_excl_sum,width=4,height=3)
+
+
+# other significance tests ----
+### ADONIS - another test to look for compositional differences, with similar results to above.(from Supp et al 2012)
+# winter
+#win.spp.canb = vegdist(win.species, method = "canb")
+#win.canb = adonis(win.spp.canb ~ win.trt, permutation=1000)
+#win.canb
---
+++ pre-flip_differences.R
@@ -0,0 +1,145 @@
+# ========================================================================
+# kruskal-wallis test ----
+# EMC 8/13/18
+# we just want to know if there are differences in treatments before flip. we don't need to model trends/seasonality
+# two sample t-test wont work -- data is not normal
+# Used Kruskal-Wallis to test for significant difference, and pairwise wilcox to see what the differences are
+# http://www.sthda.com/english/wiki/kruskal-wallis-test-in-r
+# I also used k-w to test for differences between plots within treatments. none were (removals were almost significant for energy and dipos)
+
+# dipos ---- 
+df1 = read.csv('Dipo_counts.csv')
+dat1 = prepare_dataframe(df1) %>% filter(ordereddate<22)
+
+# check that data within factor levels is normal
+dip_control = dplyr::filter(dat1,treatment=='CC')
+dip_excl = dplyr::filter(dat1,treatment=='EC')
+dip_rem = dplyr::filter(dat1,treatment=='XC')
+
+ggqqplot(dip_rem$n)
+shapiro.test(dip_control$n)
+# none of the treatments is normal. need a kruskal-wallis test 
+kruskal.test(n~treatment, data=dat1)
+# ok there are differences. use pairwise wilcox to see what differences are
+pairwise.wilcox.test(dat1$n, dat1$treatment,
+                     p.adjust.method = "bonferroni")
+# there are pairwise differences between all 3 treatments
+
+dipobox = ggplot(dat1,aes(x=treatment,y=n, colour=treatment)) + 
+  geom_boxplot() +
+  ggtitle('Kangaroo rat abundances 2013-2015') +
+  scale_colour_brewer(name = 'Treatment', type = 'qual', palette = 'Dark2') +
+  labs(y = 'Count', x = NULL) +
+  scale_x_discrete(breaks=c("CC","EC","XC"),
+                   labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
+  theme(legend.position = 'none')
+dipobox
+
+dat1 %>%
+  group_by(treatment) %>%
+  summarise(n = mean(n), sd = sd(n))
+
+#ggsave('DipoCountBoxplot.png',dipobox,width=6,height=2.5)
+
+# small granivores ----
+df = read.csv('SmallGranivores.csv')
+dat = prepare_dataframe(df) %>% filter(ordereddate<22)
+
+# check that data within factor levels is normal
+sm_control = dplyr::filter(dat,treatment=='CC')
+sm_excl = dplyr::filter(dat,treatment=='EC')
+sm_rem = dplyr::filter(dat,treatment=='XC')
+
+ggqqplot(sm_control$n)
+shapiro.test(sm_rem$n)
+# exclosures are normal, removals and controls are not. need a kruskal-wallis test 
+kruskal.test(n~treatment, data=dat)
+# ok there are differences. use pairwise wilcox to see what differences are
+pairwise.wilcox.test(dat$n, dat$treatment,
+                     p.adjust.method = "bonferroni")
+# there are pairwise differences between all 3 treatments
+
+smgranbox = ggplot(dat,aes(x=treatment,y=n, colour=treatment)) + 
+  geom_boxplot() +
+  ggtitle('Small granivore abundances 2013-2015') +
+  scale_colour_brewer(name = 'Treatment', type = 'qual', palette = 'Dark2') +
+  labs(y = 'Count', x = NULL) +
+  scale_x_discrete(breaks=c("CC","EC","XC"),
+                   labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
+  theme(legend.position = 'none')
+smgranbox
+
+dat %>%
+  group_by(treatment) %>%
+  summarise(n = mean(n), sd = sd(n))
+
+#ggsave('SmallGranivoreBoxplot.png',smgranbox,width=6,height=2.5)
+
+# energy ---- 
+df2 = read.csv('TotalCommunityEnergy.csv')
+dat2 = prepare_dataframe(df2) %>% filter(ordereddate<22)
+
+# check that data within factor levels is normal
+en_control = dplyr::filter(dat2,treatment=='CC')
+en_excl = dplyr::filter(dat2,treatment=='EC')
+en_rem = dplyr::filter(dat2,treatment=='XC')
+
+ggqqplot(en_rem$n)
+shapiro.test(en_rem$n)
+# exclosures and control are normal, removals are not. need a kruskal-wallis test 
+kruskal.test(n~treatment, data=dat2)
+# ok there are differences. use pairwise wilcox to see what differences are
+pairwise.wilcox.test(dat2$n, dat2$treatment,
+                     p.adjust.method = "bonferroni")
+# there are pairwise differences between all 3 treatments
+
+energybox = ggplot(dat2,aes(x=treatment,y=n, colour=treatment)) + 
+  geom_boxplot() +
+  ggtitle('Metabolic Flux 2013-2015') +
+  scale_colour_brewer(name = 'Treatment', type = 'qual', palette = 'Dark2') +
+  labs(y = 'Metabolic Flux', x = NULL) +
+  scale_x_discrete(breaks=c("CC","EC","XC"),
+                   labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
+  theme(legend.position = 'none')
+energybox
+
+dat2 %>%
+  group_by(treatment) %>%
+  summarise(n = mean(n), sd = sd(n))
+
+#ggsave('TotalEnergyBoxplot.png',energybox,width=6,height=2.5)
+
+
+# species richness ---- 
+df3 = read.csv('SpeciesRichness.csv')
+dat3 = prepare_dataframe(df3) %>% filter(ordereddate<22)
+
+# check that data within factor levels is normal
+sr_control = dplyr::filter(dat3,treatment=='CC')
+sr_excl = dplyr::filter(dat3,treatment=='EC')
+sr_rem = dplyr::filter(dat3,treatment=='XC')
+
+ggqqplot(sr_rem$n)
+shapiro.test(sr_rem$n)
+# exclosures and control are normal, removals are not. need a kruskal-wallis test 
+kruskal.test(n~treatment, data=dat3)
+# ok there are differences. use pairwise wilcox to see what differences are
+pairwise.wilcox.test(dat3$n, dat3$treatment,
+                     p.adjust.method = "bonferroni")
+# there are pairwise differences between all 3 treatments
+
+richbox = ggplot(dat3,aes(x=treatment,y=n, colour=treatment)) + 
+  geom_boxplot() +
+  ggtitle('Species Richness 2013-2015') +
+  scale_colour_brewer(name = 'Treatment', type = 'qual', palette = 'Dark2') +
+  labs(y = '# Species', x = NULL) +
+  scale_x_discrete(breaks=c("CC","EC","XC"),
+                   labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
+  theme(legend.position = 'none')
+richbox
+
+dat3 %>%
+  group_by(treatment) %>%
+  summarise(n = mean(n), sd = sd(n))
+
+#ggsave('SpeciesRichnessBoxplot.png',richbox,width=6,height=2.5)
