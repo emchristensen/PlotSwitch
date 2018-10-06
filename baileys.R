d3236ed97add936b2b7bf5a55e319eb668aefd82
Erica Christensen <erica.christensen@weecology.org>
2018-10-06 19:13:36 -0400

    reworking baileys.r

--- baileys.R
+++ baileys.R
@@ -11,24 +11,7 @@ dat = get_data(startdate = "2013-03-11",min_num_plots = 24,treatments = c('CC','
 # just PB abundance
 pb = make_N_data(species = 'PB',dat)
 
-# monthly mean
-pbmean = aggregate(pb$n,by=list(censusdate = pb$censusdate, treatment=pb$treatment), FUN=mean)
 
-# plot data
-baileys = ggplot(pb, aes(x = censusdate, y =n, colour = treatment)) +
-  geom_jitter(height=.1,width=.3) +
-  geom_line(aes(x=censusdate,y=x,colour=treatment),data=pbmean,size=1) +  #geom_smooth(method='loess',se = TRUE) +
-  theme(legend.position = 'right') +
-  labs(y = 'Abundance', x = NULL) +
-  geom_vline(xintercept=as.Date('2015-04-10')) +
-  scale_colour_manual(name = 'Treatment', values = cbPalette,
-                      breaks=c("CC","EC","XC"),
-                      labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
-  scale_fill_manual(name = 'Treatment', values = cbPalette, 
-                    breaks=c("CC","EC","XC"),
-                    labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) 
-baileys
-ggsave('BaileysAbundance.png',baileys,width=6,height=2)
 
 ## Alternate version using model estimates
 ## add variabels for modelling
@@ -93,9 +76,27 @@ baileys <- ggplot(pb, aes(x = censusdate, y = n, colour = treatment)) +
                       labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) 
 baileys
 
-ggsave('BaileysAbundance.png', baileys, width = 6, height = 2)
+ggsave('BaileysAbundanceGAM.png', baileys, width = 6, height = 2)
 
-# PB disappear from EC plots but not EE
-# PF maybe a difference?
-# PP, PE, RM, BA no difference
-# PM, PL, RF, RO not enough data
+
+
+# ============================================================================================
+# Old code: plot just monthly mean data without GAM 
+## monthly mean
+# pbmean = aggregate(pb$n,by=list(censusdate = pb$censusdate, treatment=pb$treatment), FUN=mean)
+# 
+# # plot data
+# baileys = ggplot(pb, aes(x = censusdate, y =n, colour = treatment)) +
+#   geom_jitter(height=.1,width=.3) +
+#   geom_line(aes(x=censusdate,y=x,colour=treatment),data=pbmean,size=1) +  #geom_smooth(method='loess',se = TRUE) +
+#   theme(legend.position = 'right') +
+#   labs(y = 'Abundance', x = NULL) +
+#   geom_vline(xintercept=as.Date('2015-04-10')) +
+#   scale_colour_manual(name = 'Treatment', values = cbPalette,
+#                       breaks=c("CC","EC","XC"),
+#                       labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) +
+#   scale_fill_manual(name = 'Treatment', values = cbPalette, 
+#                     breaks=c("CC","EC","XC"),
+#                     labels=c("long-term\n control", "kangaroo rat\n removal", "rodent\n removal")) 
+# baileys
+# ggsave('BaileysAbundance.png',baileys,width=6,height=2)
