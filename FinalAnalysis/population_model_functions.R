25dc0e753b0befb75d3d9cace895eb886907144d
emchristensen <erica.christensen@weecology.org>
2019-04-23 16:40:55 -0600

    start population_models and population_model_functions for doing survival models

---
+++ FinalAnalysis/population_model_functions.R
@@ -0,0 +1,309 @@
+
+#' @description used by individual_tag_cleanup: will split apart multiple uses of same tag number if captures are
+#'              too far apart in time to be considered the same individual
+#' @param indiv
+#' @param datframe
+#' 
+split_dup_tags = function(indiv,datframe,interval) {
+  # Function for splitting instances of same tag being used in different time periods
+  
+  indiv = indiv[order(indiv$date),]           # put in chronological order if not already
+  tagrep = 0
+  for (n in 2:length(indiv$date)) {
+    if (indiv$date[n]-indiv$date[n-1] > interval) {   # if interval between captures > 1.5 years,
+      tagrep = tagrep+1
+      ids = indiv$Record_ID[n:length(indiv$date)] # find record IDs of subsequent entries to connect back to datframe
+      datframe[datframe$Record_ID %in% ids,'tagrep'] = tagrep 
+    }
+  }                   
+  
+  assign('datframe',datframe,envir=.GlobalEnv)  # make sure changes to datframe are applied outside this function
+}
+
+#' @param sp (character) species of interest
+#' @param dat data frame of all rodent data from PortalData
+#' @param interval maximum allowable interval between captures before the same tag number should be considered a new individual
+#' @param nulls null values
+#' 
+individual_tag_cleanup = function(sp, dat, interval=5*365, nulls = c(0,-1,'','000000',9999,NA)) {
+  sp_dat = dplyr::filter(dat, species %in% sp)
+  
+  # remove untagged individuals
+  sp_dat = sp_dat[!is.na(sp_dat$tag),]
+  
+  # add column for full date
+  sp_dat$date = as.Date(paste(sp_dat$year,sp_dat$month,sp_dat$day,sep='-'), format='%Y-%m-%d')
+  
+  #----------------------------------------------------------------------------------------
+  # Strip tag and date data from larger data frame
+  
+  # create data frames with just record_ID, date, and associated tag numbers (tag, ltag, prevrt, prevlet)
+  rtag = sp_dat[,c('recordID','date','tag')]
+  ltag = sp_dat[,c('recordID','date','ltag')]
+  prtag = sp_dat[,c('recordID','date','prevrt')]
+  pltag = sp_dat[,c('recordID','date','prevlet')]
+  
+  # remove nulls from these data frames
+  rtag = rtag[!rtag$tag %in% nulls,] 
+  ltag = ltag[!ltag$ltag %in% nulls,]
+  prtag = prtag[!prtag$prevrt %in% nulls,]
+  pltag = pltag[!pltag$prevlet %in% nulls,]
+  
+  # split up previous tags (may contain multiple tags in tag column)
+  recordID = c()
+  date = c()
+  tag = c()
+  for (n in 1:length(prtag$prevrt)) {
+    rts = unlist(strsplit(prtag$prevrt[n],','))
+    for (m in 1:length(rts)) {
+      recordID = append(recordID,prtag$recordID[n])
+      date = append(date,prtag$date[n])
+      tag = append(tag,rts[m])
+    }
+  }
+  prt = data.frame(recordID,date,tag,stringsAsFactors=F)
+  
+  recordID = c()
+  date = c()
+  tag = c()
+  for (n in 1:length(pltag$prevlet)) {
+    lts = unlist(strsplit(pltag$prevlet[n],','))
+    for (m in 1:length(lts)) {
+      recordID = append(recordID,pltag$recordID[n])
+      date = append(date,pltag$date[n])
+      tag = append(tag,lts[m])
+    }
+  }
+  plt = data.frame(recordID,date,tag,stringsAsFactors=F)
+  
+  # Put data frames together into one big data frame
+  ltag = dplyr::rename(ltag, tag=ltag)
+  datframe = rbind(rtag, ltag, prt, plt)
+  datframe$tagrep = 0
+  
+  # strip leading or trailing whitespace from tags
+  datframe$tag <- stringr::str_trim(datframe$tag,side='both')
+  
+  # make sure all tags are 6 digits (differences in data entry, 001262 versus 1262)
+  datframe$tag <- stringr::str_pad(datframe$tag, 6, pad = "0")
+  
+  #----------------------------------------------------------------------------------------------
+  # Split up tags used multiple times in data set
+  
+  # Loop through unique tag numbers and time-splitting function
+  for (t in unique(datframe$tag)) {
+    indiv = dplyr::filter(datframe, tag==t) %>% arrange(date)
+    if (any(diff(indiv$date)>interval))  {        # if time between captures > 1.5 years, run split_dup_tags
+      split_dup_tags(indiv,datframe,interval)
+      print(paste('duplicate tag? ',t))
+    }
+  }
+  
+  datframe$tagunique = paste(datframe$tag,'_',datframe$tagrep,sep='')
+  
+  #---------------------------------------------------------------------------------------
+  # merge unique tags with original records in data set
+  
+  R_ID = c()
+  utags = c()
+  for (id in unique(datframe$recordID)) {
+    record = datframe[datframe$recordID==id,]
+    R_ID = append(R_ID,id)
+    utags = append(utags,paste(record$tagunique,collapse=','))
+  }
+  
+  # Data frame of record IDs and unique tags
+  uniquetags = data.frame(recordID=R_ID, tagunique = utags,stringsAsFactors=F)
+  
+  # Merge with main data frame
+  newdatframe = merge(sp_dat,uniquetags,by='recordID')
+  
+  
+  #--------------------------------------------------------------------------------------
+  # Isolate unique tag numbers (vertices)
+  alltags = unique(datframe$tagunique)
+  
+  #--------------------------------------------------------------------------------------
+  # Isolate connections between tags (L/R/previous) for edges
+  
+  s = c()   # empty list for storing sets of associated tags
+  
+  for (n in 1:length(sp_dat$tag)) {        # Loop through all records in data frame
+    edge = as.character(unlist(strsplit(newdatframe$tagunique[n],',')))
+    edge = edge[!is.na(edge)]          # remove NAs
+    edge = edge[!edge %in% nulls]      # remove nulls
+    s[n] = list(edge)
+  }
+  
+  #--------------------------------------------------------------------------------------
+  # Make it a graph (igraph package)
+  
+  sp_graph = graph.empty() + vertices(alltags)
+  
+  for (edge in s) {
+    sp_graph = sp_graph + path(as.vector(edge))
+  }
+  
+  #decomposes into connected components (independent subgraphs) each representing an individual
+  graphs = igraph::decompose.graph(sp_graph) 
+  
+  
+  #-----------------------------------------------------------------------------------
+  # Group together records associated with each individual (subgraph)
+  
+  # Make data frame of all tag numbers and which group number (individual) they belong to
+  tag = c()
+  group = c()
+  for (n in 1:length(graphs)) {
+    vertices = as.vector(unlist(vertex.attributes(graphs[[n]])))    # vector of tags that represent an individual
+    tag = append(tag,vertices)
+    group = append(group,rep(n,length(vertices)))
+  }
+  
+  taggroups = data.frame(tagunique=tag,group,stringsAsFactors=F)
+  
+  # Merge tag/group data frame with capture data
+  for (n in 1:length(newdatframe$tagunique)) {
+    tgs = as.character(unlist(strsplit(newdatframe$tagunique[n],',')))
+    newdatframe$tagunique1[n] = tgs[1]
+  }
+  new_sp_dat = merge(newdatframe,taggroups,by.x='tagunique1',by.y='tagunique')
+  
+  # sort by time (record id)
+  new_sp_dat = new_sp_dat[order(new_sp_dat$recordID),]
+  
+  
+  
+  selectdat = new_sp_dat[,c('recordID','month','day','year','period','plot','species','sex','age',
+                            'reprod','testes','vagina','pregnant','nipples','lactation','hfl',
+                            'wgt','tag','note2','ltag','note3','prevrt','prevlet','note5','date',
+                            'tagunique','group')]
+  
+  return(selectdat)
+}
+
+
+#' @description creates capture history of individual rodents
+#' @param dat data frame of rodent captures; after cleaning by individual_tag_cleanup
+#' Written by Ellen Bledsoe
+create_trmt_hist = function(dat) {
+  individuals <- unique(dat$group)
+  prd <- seq(min(dat$period), max(dat$period))
+  
+  MARK_data = data.frame("captures" = 1,
+                         "censored" = 1,
+                         "tags" = 1)
+  
+  outcount = 0
+  
+  for (t in 1:length(individuals)) {
+    
+    capture_history = "" #create empty string
+    
+    for (p in 1:length(prd)) {
+      
+      tmp <- dat[which(dat$group == individuals[t] & dat$period == prd[p]),]
+      
+      if (nrow(tmp) == 0) {
+        state = "0"
+      } else {
+        if (tmp$treatment == 'CC') {
+          state = "A"
+        } else if (tmp$treatment == 'EC') {
+          state = "B"
+        } else if (tmp$treatment == 'XC') {
+          state = "C"
+        } else if (tmp$treatment == 'XX') {
+          state = "D"
+        } else if (tmp$treatment == 'EE') {
+          state = 'E'
+        } else if (tmp$treatment == 'CE') {
+          state = 'G'
+        } else if (tmp$treatment == 'CX') {
+          state = 'H'
+        }
+      }
+      capture_history = paste0(capture_history, state)
+    }
+    
+    tmp2 <- which(dat$group == individuals[t])
+    censored = 1
+    
+    outcount = outcount + 1
+    MARK_data[outcount, ] <- c(capture_history, censored, individuals[t])
+    
+  }
+  
+  return(MARK_data)
+  
+}
+
+
+#' written by Ellen Bledsoe
+run.ms <- function(S_dot = list(formula = ~ 1), 
+                   S_stratum = list(formula =  ~ -1 + stratum + after_switch), 
+                   p_dot = list(formula =  ~ 1), 
+                   p_stratum = list(formula =  ~ -1 + stratum + after_switch), 
+                   Psi_s = list(formula =  ~ -1 + stratum:tostratum + after_switch, link = "logit")) {
+  
+  # RMark function for Portal data
+  if (is.null(S_dot)) {
+    S.stratum = S_stratum
+  } else if (is.null(S_stratum)) {
+    S.dot = S_dot
+  } else {
+    S.stratum = S_stratum
+    S.dot = S_dot
+  }
+  
+  if (is.null(p_dot)) {
+    p.stratum = p_stratum
+  } else if (is.null(p_stratum)) {
+    p.dot = p_dot
+  } else {
+    p.stratum = p_stratum
+    p.dot = p_dot
+  }
+  
+  Psi.s = Psi_s
+  
+  # Create model list and run assortment of models
+  ms.model.list = create.model.list("Multistrata")
+  
+  ms.results = mark.wrapper(ms.model.list,
+                            data = ms.pr, ddl = ms.ddl,
+                            options="SIMANNEAL")
+  
+  # Return model table and list of models
+  return(ms.results)
+  
+}
+
+#' written by Ellen Bledsoe
+prep_RMark_data_for_plotting <- function(data){
+  
+  # prep RMark results for plotting
+  data$time = c("Before", "After", "Before", "After", "Before", "After", NA, 
+                "Before", "After", "Before", "After", "Before", "After",
+                "Before", "After", "Before", "After", "Before", "After")
+  
+  # add descriptive columns
+  data$metric = rep("S", nrow(data))
+  data$metric[7] = "p"
+  data$metric[8:19] = "Psi"
+  
+  data$stratum = c("A", "A", "B", "B", "C", "C", NA, 
+                   "AB", "AB",  "AC", "AC", "BA", "BA",
+                   "BC", "BC", "CA", "CA", "CB", "CB")
+  data$Treatment = c("Control", "Control", "KR Exclosure", "KR Exclosure", "Removal", "Removal", NA,
+                     "Control to KR Exclosure", "Control to KR Exclosure", "Control to Removal", "Control to Removal",
+                     "KR Exclosure to Control", "KR Exclosure to Control", "KR Exclosure to Removal", "KR Exclosure to Removal",
+                     "Removal to Control", "Removal to Control", "Removal to KR Exclosure", "Removal to KR Exclosure")
+  
+  data <- data %>% 
+    filter(metric != "p", stratum == "A" | stratum == "B" | stratum == "AB" | stratum == "BA")
+  data$time <- factor(data$time, levels = c("Before", "After"))
+  
+  return(data)
+  
+}
---
+++ FinalAnalysis/population_models.R
@@ -0,0 +1,323 @@
+# PP Shifts Paper
+# Ellen K. Bledsoe, with code from S. Supp
+# March 7, 2018
+# Modified EMC for Plot Switch project 4/22/19
+
+# LIBRARIES and SOURCE CODE #
+
+library(tidyverse)
+library(portalr)
+library(RCurl)
+library(plotrix)
+library(RMark)
+library(forecast)
+library(nlme)
+library(patchwork) # devtools::install_github("thomasp85/patchwork")
+library(rapportools)
+
+source("FinalAnalysis/population_model_functions.r")
+
+# DATA FILES (PortalData folder was downloaded as part of create_data_series.R script)
+
+# rodent file
+rdat <- read.csv('PortalData/Rodents/Portal_rodent.csv', header = TRUE, na.strings = c(""), stringsAsFactors = FALSE)
+
+# species file
+#sdat <- read.csv('PortalData/Rodents/Portal_rodent_species.csv', header = TRUE, na.strings = c(""), stringsAsFactors = FALSE)
+
+# trapping data
+tdat <- read.csv('PortalData/Rodents/Portal_rodent_trapping.csv', header = TRUE, stringsAsFactors = FALSE)
+
+# plot treatments: type of switch
+#pdat <- read.csv('PortalData/SiteandMethods/Portal_plots.csv', header = TRUE, stringsAsFactors = FALSE)
+pdat <- data.frame(plot = 1:24, treatment = c('CC','CE','EE','CC','XC','EC',
+                                              'XC','CE','CX','XX','CC','CX',
+                                              'EC','CC','EE','XX','CC','EC',
+                                              'EE','EE','EE','CE','XX','XC'))
+
+##########################################################
+# DATA PREP
+##########################################################
+
+#---------------------------------------------------------
+# Clean the Data
+#---------------------------------------------------------
+rdat_filtered = dplyr::filter(rdat, period>=415, plot %in% c(4,11,14,17,6,13,18,5,7,24))
+
+dmdat = individual_tag_cleanup('DM', rdat_filtered)
+dipodat = individual_tag_cleanup(sp=c('DO','DM','DS'), rdat_filtered)
+
+dmdat_trt = merge(dmdat, pdat, by=c('plot'))
+
+#################################################################################
+### Warning: this will take a long time to run and maybe crash your computer! ###
+#################################################################################
+#                                                                               #
+#       The `create_trmt_hist` function to create mark_trmt_all will            #
+#       take a long time to run. If you don't want to run it, you can           #
+#       read in the results from the GitHub repo (line 233-4) and then          #
+#       run the RMark code -OR- skip to line 286 for all RMark results          #
+#                                                                               #
+#################################################################################
+
+mark_dm = create_trmt_hist(dmdat_trt)
+# resulting warnings are probably for animals captured twice in same sampling period
+
+# mark_trmt_all = create_trmt_hist(PP_only, tags_all, periods_all)
+
+# load in capture histories if already created
+#all_hist <- getURL("https://raw.githubusercontent.com/bleds22e/PP_shifts/master/data/PP_capture_history_all_20180711.csv")
+#mark_trmt_all <- read.csv(text = all_hist, header = TRUE, stringsAsFactors = FALSE)
+
+# prep data for RMark
+all_ms <- select(mark_dm, captures) %>% dplyr::rename("ch" = "captures")
+first_per <- min(dmdat_trt$period)
+
+# Process data
+ms.pr = process.data(all_ms, begin.time = first_per, model = "Multistrata")
+ 
+# Create default design data
+ ms.ddl = make.design.data(ms.pr)
+ 
+# add design covariates for PB era
+after_switch = as.factor(437:476)
+ 
+ms.ddl$S$after_switch = 0
+ms.ddl$S$after_switch[ms.ddl$S$time %in% after_switch] = 1
+ 
+ms.ddl$p$after_switch = 0
+ms.ddl$p$after_switch[ms.ddl$p$time %in% after_switch] = 1
+ 
+ms.ddl$Psi$after_switch = 0
+ms.ddl$Psi$after_switch[ms.ddl$Psi$time %in% after_switch] = 1
+
+# Run the models and examine the output
+
+#################################################################################
+### Warning: this will take a long time to run and maybe crash your computer! ###
+#################################################################################
+#                                                                               #
+#       Running the `run.ms` function to get ms.results will take a             #
+#       long time to run. If you don't want to run this yourself, you           #
+#       can read in the RMark results from GitHub instead (line 286)            #
+#                                                                               #
+#################################################################################
+
+ MarkViewer="open -a TextEdit" # edit to make results pop up on a Mac
+
+ ms.results = run.ms(S_dot = NULL,
+                     S_stratum = list(formula = ~ -1 + stratum * after_switch),
+                     p_dot = list(formula = ~ 1),
+                     p_stratum = NULL,
+                     Psi_s = list(formula =  ~ -1 + stratum:tostratum * after_switch, link = "logit"))
+ms.results
+names(ms.results)
+ 
+ms.summary = ms.results$S.stratum.p.dot.Psi.s
+ms.summary
+rmark_results <- ms.summary$results$real
+write.csv(ms.summary$results$real, "Data/MARKdatatop_model_summary_[DATE].csv")
+
+# read in Mark results if skipping that section
+rmark_results <- read.csv("../PP_shifts/data/top_model_summary_20190416.csv", stringsAsFactors = FALSE)
+
+# plot RMark results
+
+plot_rmark <- prep_RMark_data_for_plotting(rmark_results)
+
+(plot2a <- plot_estimated_survival(plot_rmark))
+
+(plot2b <- plot_transition_probability(plot_rmark))
+
+#------------------------------------------------------------
+# Number of New PP Individuals Showing Up on Plots
+#------------------------------------------------------------
+
+# make empty dataframe
+first_period <- setNames(data.frame(matrix(ncol = 21, nrow = 0)), names(PP_only))
+
+# create dataframe of only first period each tag is present
+for (i in 1:length(tags_all)){
+  tmp <- PP_only[PP_only$tag == tags_all[i],] # rows for a given tag
+  tmp2 <- tmp[tmp$period == min(tmp$period),] # row with earliest period
+  first_period <- rbind(first_period, tmp2)   # add to new dataframe
+}
+
+# total number new PPs (avg plot sum by year)
+new_PP_per_plot <- first_period %>%
+  filter(plot_type != "Removal") %>%
+  group_by(plot, month, year, plot_type) %>%
+  summarise(count = n(species)) %>%
+  ungroup()
+
+new_PP_per_plot <- new_PP_per_plot %>% 
+  group_by(year, plot_type) %>% 
+  summarise(sum_by_year = sum(count))
+
+new_PP_per_plot$time_point <- NA
+
+for (i in 1:nrow(new_PP_per_plot)) {
+  if (new_PP_per_plot$year[i] <= 1997) {
+    new_PP_per_plot$time_point[i] = "Before"
+  } else {
+    new_PP_per_plot$time_point[i] = "After"
+  }
+}
+
+anova2w <- aov(sum_by_year ~ plot_type * time_point, data = new_PP_per_plot)
+summary(anova2w)
+
+# plot new PP individuals
+(plot2c <- plot_new_PP_individuals(new_PP_per_plot))
+
+# Make Figure 2
+(plot2 <- plot2a + plot2b - plot2c + plot_layout(ncol = 1))
+
+# ggsave("figures/1989-2010/Figure2.png", plot2, width = 6, height = 7, dpi = 600)
+# ggsave("figures/1989-2010/Figure2.tiff", plot2,
+#        width = 6, height = 7, dpi = 600, compression = "lzw")
+
+
+### RUN ANOVAS ###
+# Things to deal with:
+#   - which way to summarize the data
+#   - what about after 2010? seems to be washing out treatment and interaction
+
+# 2-way ANOVA by monthly count
+
+# new_PP_per_plot$time_point <- NA
+# 
+# for (i in 1:nrow(new_PP_per_plot)) {
+#   if (new_PP_per_plot$year[i] < 1997) {
+#     new_PP_per_plot$time_point[i] = "Before"
+#   } else if (new_PP_per_plot$year[i] > 1997){
+#     new_PP_per_plot$time_point[i] = "After"
+#   } else {
+#     if (new_PP_per_plot$month[i] < 7){
+#       new_PP_per_plot$time_point[i] = "Before"
+#     } else {
+#       new_PP_per_plot$time_point[i] = "After"
+#     }
+#   }
+# }
+# 
+# new_PP_per_plot <- filter(new_PP_per_plot, year <= 2010)
+# anova2w <- aov(count ~ plot_type * time_point, data = new_PP_per_plot)
+# summary(anova2w)
+# 
+
+
+# 2-way ANOVA by year
+
+# new_PP_per_plot <- new_PP_per_plot %>% 
+#   group_by(year, plot_type) %>% 
+#   summarise(count = sum(count))
+# 
+# new_PP_per_plot$time_point <- NA
+# 
+# for (i in 1:nrow(new_PP_per_plot)) {
+#   if (new_PP_per_plot$year[i] <= 1997) {
+#     new_PP_per_plot$time_point[i] = "Before"
+#   } else {
+#     new_PP_per_plot$time_point[i] = "After"
+#   }
+# }
+# 
+# new_PP_per_plot <- filter(new_PP_per_plot, year <= 2010)
+# anova2w <- aov(count ~ plot_type * time_point, data = new_PP_per_plot)
+# summary(anova2w)
+# 
+# ggplot(data = new_PP_per_plot, aes(x = year, y = count, color = plot_type)) +
+#   geom_point() +
+#   geom_line() +
+#   theme_bw()
+
+# 2-way ANOVA by avg per plot per year
+
+# new_PP_per_plot_summary$time_point <- NA
+# 
+# for (i in 1:nrow(new_PP_per_plot_summary)) {
+#   if (new_PP_per_plot_summary$year[i] <= 1997) {
+#     new_PP_per_plot_summary$time_point[i] = "Before"
+#   } else {
+#     new_PP_per_plot_summary$time_point[i] = "After"
+#   }
+# }
+# 
+# new_PP_per_plot_summary <- filter(new_PP_per_plot_summary, year <= 2010)
+# anova2w <- aov(avg_plot_sum_by_year ~ plot_type * time_point, data = new_PP_per_plot_summary)
+# summary(anova2w)
+
+#############################################################
+# System-level Aspects of Patch Preference
+#############################################################
+
+# download biomass data by plot from portalr
+biomass_data <- portalr::biomass(path = "repo", level = "Plot")
+energy_data <- portalr::energy(path = "repo", level = "Plot")
+
+# select certain treatments and filter by time
+biomass_dat <- biomass_data %>%
+  filter(treatment == "control" | treatment == "exclosure", 
+         period >= 118 & period <= 433) # get the right time periods
+energy_dat <- energy_data %>%
+  filter(treatment == "control" | treatment == "exclosure", 
+         period >= 118 & period <= 433) 
+
+
+# add a year column for later summarization
+year_prd_pairs <- unique(tdat[,c("year", "period")]) # get associated years and periods
+biomass_dat$year = NA
+energy_dat$year = NA
+
+for (i in 1:nrow(biomass_dat)){
+  prd <- biomass_dat$period[i]
+  biomass_dat$year[i] = year_prd_pairs$year[year_prd_pairs$period == prd]
+}
+
+for (i in 1:nrow(energy_dat)){
+  prd <- energy_dat$period[i]
+  energy_dat$year[i] = year_prd_pairs$year[year_prd_pairs$period == prd]
+}
+
+#------------------------------------------------------------
+# Biomass Ratios
+#------------------------------------------------------------
+
+# sum across rows and rename column
+biomass_dat_rowSums <- as.data.frame(rowSums(biomass_dat[,4:24]))
+colnames(biomass_dat_rowSums) <- c("rowSums")
+
+# summarise biomass to get total by period and plot type
+biomass_total <- cbind(biomass_dat, biomass_dat_rowSums) %>%
+  group_by(year, treatment) %>%
+  summarise(totals = sum(rowSums))
+
+# change the data structure to run the linear model
+biomass_spread <- tidyr::spread(biomass_total, treatment, totals)
+
+# ratio
+biomass_ratio <- biomass_spread %>% mutate(EX_to_CO_ratio = exclosure/control)
+
+(plot3 <- plot_biomass_ratio(biomass_ratio))
+# ggsave("figures/1989-2010/Figure3.png", plot3, width = 3.5, height = 3, dpi = 600)
+
+## ENERGY ##
+
+# sum across rows and rename column
+energy_dat_rowSums <- as.data.frame(rowSums(energy_dat[,4:24]))
+colnames(energy_dat_rowSums) <- c("rowSums")
+
+# summarise energy to get total by period and plot type
+energy_total <- cbind(energy_dat, energy_dat_rowSums) %>%
+  group_by(year, treatment) %>%
+  summarise(totals = sum(rowSums))
+
+# change the data structure to run the linear model
+energy_spread <- tidyr::spread(energy_total, treatment, totals)
+
+# ratio
+energy_ratio <- energy_spread %>% mutate(EX_to_CO_ratio = exclosure/control)
+
+(plot3_energy <- plot_energy_ratio(energy_ratio))
+# ggsave("figures/1989-2010/Figure3_energy.png", plot3_energy, width = 3.5, height = 3, dpi = 600)
